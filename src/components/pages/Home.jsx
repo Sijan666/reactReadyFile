0b769a2a6d734ed2ecdd7e1e93fdd690bff32fad
Sijan666 <666majharulislam@gmail.com>
2026-02-20 23:31:40 +0600

    changes in home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,24 +1,11 @@
 
-
-
 import Banner from "../layouts/Banner"
-import About from "./About"
-import Service from './Service'
-import Blog from './Blog'
-import ContactUs from './ContactUs'
-
-
-
 
 
 const Home = () => {
     return (
         <>
         <Banner/>
-        <About/>
-        <Service/>
-        <Blog/>
-        <ContactUs/>
         </>
     )
 }
